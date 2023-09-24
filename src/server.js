7581690c4d7970a33fbbf66e4407319c2edd1f23
alilgae <ert2423@rit.edu>
2023-09-24 19:24:13 -0400

    index page  and css displayed

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const htmlHandler = require('./responses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    /* '/getUsers': jsonHandler.getUsers,
+    '/updateUser': jsonHandler.updateUser,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFoundMeta,
+  */ },
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (!urlStruct[request.method]) {
+    return urlStruct.HEAD.notFound(request, response);
+  }
+
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    return urlStruct[request.method][parsedUrl.pathname](request, response);
+  }
+
+  return urlStruct[request.method].notFound(request, response);
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
