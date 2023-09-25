99f31af5c85c1462b44d3555ac2e809597baedef
alilgae <ert2423@rit.edu>
2023-09-24 23:18:12 -0400

    response handler

--- src/responses.js
+++ src/responses.js
@@ -1,20 +1,77 @@
-const fs = require('fs');
+const toXML = (messageJSON) => {
+  let XML = '<response>';
+  XML += `<message>${messageJSON.message}</message>`;
+  XML += messageJSON.id ? `<id>${messageJSON.id}</id></response>` : '</response>';
+  return XML;
+};
 
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-const css = fs.readFileSync(`${__dirname}/../client/style.css`);
-
-const getIndex = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.write(index);
+const sendResponse = (request, response, status, type, content) => {
+  response.writeHead(status, { 'Content-Type': type });
+  response.write(type === 'application/json' ? JSON.stringify(content) : toXML(content));
   response.end();
 };
 
-const getCSS = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/css' });
-  response.write(css);
-  response.end();
+const handler = (request, response, value, type) => {
+  let statusCode = 404;
+  const responseJSON = {
+    message: 'This is a successful response',
+  };
+  switch (value) {
+    case '/success':
+      // 200
+      statusCode = 200;
+      break;
+
+    case '/badRequest':
+      // 400
+      responseJSON.message = 'Missing valid querey parameter set to true.';
+      responseJSON.id = 'badRequest';
+      statusCode = 400;
+      // 200
+      statusCode = 200;
+      break;
+
+    case '/unauthorized':
+      // 401
+      responseJSON.message = 'Missing loggedIn querey parameter set to yes.';
+      responseJSON.id = 'unathorized';
+      statusCode = 401;
+      // 200
+      statusCode = 200;
+      break;
+
+    case '/forbidden':
+      // 403
+      responseJSON.message = 'You do not have access to this content.';
+      responseJSON.id = 'forbidden';
+      statusCode = 403;
+      break;
+
+    case '/internal':
+      // 500
+      responseJSON.message = 'Internal server error. Something went wrong.';
+      responseJSON.id = 'internalError';
+      statusCode = 500;
+      break;
+
+    case '/notImplemented':
+      // 501
+      responseJSON.message = 'A get request for this page has not been implemented yet. Check again later for updated content.';
+      responseJSON.id = 'notImplemented';
+      statusCode = 501;
+      break;
+
+    default:
+      // 404
+      responseJSON.message = 'The page you are looking for was not found.';
+      responseJSON.id = 'notFound';
+      statusCode = 404;
+      break;
+  }
+
+  return sendResponse(request, response, statusCode, type, responseJSON);
 };
 
 module.exports = {
-  getIndex, getCSS,
+  handler,
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
-const htmlHandler = require('./responses.js');
+const htmlHandler = require('./htmlResponses.js');
+const handler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -8,28 +9,24 @@ const urlStruct = {
   GET: {
     '/': htmlHandler.getIndex,
     '/style.css': htmlHandler.getCSS,
-    /* '/getUsers': jsonHandler.getUsers,
-    '/updateUser': jsonHandler.updateUser,
-    notFound: jsonHandler.notFound,
   },
-  HEAD: {
-    '/getUsers': jsonHandler.getUsersMeta,
-    notFound: jsonHandler.notFoundMeta,
-  */ },
+
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
   if (!urlStruct[request.method]) {
-    return urlStruct.HEAD.notFound(request, response);
+    // return urlStruct.HEAD.notFound(request, response);
   }
 
   if (urlStruct[request.method][parsedUrl.pathname]) {
     return urlStruct[request.method][parsedUrl.pathname](request, response);
   }
 
-  return urlStruct[request.method].notFound(request, response);
+  // return urlStruct[request.method].notFound(request, response);
+
+  return handler.handler(request, response, parsedUrl, 'application/json');
 };
 
 http.createServer(onRequest).listen(port, () => {
